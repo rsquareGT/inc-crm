afb092b5c09d1a19bb2bc83645057f8bc21cee17
Rameez Raja <shanu2rick@gmail.com>
2025-05-23 15:40:12 +0000

    I think only middle part 4fzyo82u9 is fine we don't need date in the end and make sure the id doesn't exists in db

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -5,5 +6,10 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-// Moved from mock-data.ts
-export const generateId = () => `id-${Math.random().toString(36).substr(2, 9)}-${Date.now()}`;
+/**
+ * Generates a 9-character random alphanumeric string.
+ * Example: '4fzyo82u9'
+ * Uniqueness in the database should be enforced by PRIMARY KEY or UNIQUE constraints on ID columns.
+ */
+export const generateId = () => Math.random().toString(36).substr(2, 9);
+
